d81a25cad81c014140febd540737bb9f20a91259
Luke Geneva <lukegeneva@gmail.com>
2017-03-03 00:14:26 -0600

    Add ability to record wins

--- seedhistory.py
+++ seedhistory.py
@@ -1,7 +1,20 @@
 class SeedHistory:
     def __init__(self):
+        MAX_SEED = 16
         self._records = {}
+        for seed in range(1, MAX_SEED+1):
+            for opponent_seed in range(1, MAX_SEED+1):
+                matchup = self._create_matchup(seed, opponent_seed)
+                self._records[matchup] = (0,0)
 
     def record(self, seed, opponent_seed):
-        matchup = (seed, opponent_seed)
-        return self._records[matchup] if matchup in self._records else (0,0)
+        matchup = self._create_matchup(seed, opponent_seed)
+        return self._records[matchup]
+
+    def record_win(self, winning_seed, losing_seed):
+        matchup = self._create_matchup(winning_seed, losing_seed)
+        wins, losses = self._records[matchup]
+        self._records[matchup] = (wins+1, losses)
+
+    def _create_matchup(self, seed, opponent_seed):
+        return (seed, opponent_seed)
--- testseedhistory.py
+++ testseedhistory.py
@@ -8,5 +8,11 @@ class TestSeedHistory(unittest.TestCase):
         record = history.record(1,1)
         self.assertEqual((0,0), record)
 
+    def test_record_win_adds_win_for_winning_seed(self):
+        history = SeedHistory()
+        history.record_win(1, 16)
+        record = history.record(1, 16)
+        self.assertEqual((1,0), record)
+
 if __name__ == '__main__':
     unittest.main()
