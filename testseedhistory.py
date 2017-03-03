9de14d79dba42333bd9f72fa7788229a6d4124a2
Luke Geneva <lukegeneva@gmail.com>
2017-03-03 00:44:04 -0600

    Refactor seed history tests

--- testseedhistory.py
+++ testseedhistory.py
@@ -4,18 +4,17 @@ from matchup import Matchup
 
 class TestSeedHistory(unittest.TestCase):
 
+    def setUp(self):
+        self.history = SeedHistory()
+        self.matchup = Matchup(1,16)
+
     def test_record(self):
-        history = SeedHistory()
-        matchup = Matchup(1,1)
-        record = history.record(matchup)
+        record = self.history.record(self.matchup)
         self.assertEqual((0,0), record)
 
     def test_record_win_adds_win_for_winning_seed(self):
-        history = SeedHistory()
-        winning_seed = 1
-        losing_seed = 16
-        history.record_win(winning_seed, losing_seed)
-        record = history.record(Matchup(winning_seed, losing_seed))
+        self.history.record_win(self.matchup.seed, self.matchup.opponent_seed)
+        record = self.history.record(self.matchup)
         self.assertEqual((1,0), record)
 
 if __name__ == '__main__':
